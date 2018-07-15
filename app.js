990519ae8f62aaac1712f89474c6e10de5c05e85
bdurb <brian@rpmgreenville.com>
2018-07-14 18:37:40 -0700

    set up dynamic url for address fetching

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-
+const address = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
