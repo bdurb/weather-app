cba5f0ec306b2c5f93078e90df565bb0660ca700
bdurb <brian@rpmgreenville.com>
2018-07-14 19:27:28 -0700

    broke out google maps api request into its own file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const request = require('request');
+
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -14,19 +16,5 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-const address = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address)
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google Servers')
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unbale to find that address');
-  } else if (body.status === 'OK') {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-  console.log(`Lng: ${body.results[0].geometry.location.lng}`);
-  }
-});
