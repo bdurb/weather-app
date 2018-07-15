eaf261274ecc7d637f087fe7072f4a4e80c70fff
bdurb <brian@rpmgreenville.com>
2018-07-14 22:17:40 -0700

    set up request to weather api

---
+++ weather/weather.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const weatherKey = require('../keys');
+
+const getWeather = (lat, lng, callback) => {
+  request({
+    url: `https://aip.forecast.io/forecast/${weatherKey}/${lat},${lng}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to Forecast.io server');
+    } else if (response.statusCode === 400) {
+      callback('unable to fetch weather');
+    } else if (response.statusCode === 200) {
+      callback(undefined, {
+        temperature: body.currently.temperature,
+        apparentTemperature: body.currently.apparentTemperature
+      });
+    }
+  });
+};
+
+module.exports.getWeather = getWeather;
